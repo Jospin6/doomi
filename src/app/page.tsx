c847f6a49657b3ac5dcc44850a3589b30a0533a7
jospin6 <jospinndagano1@gmail.com>
2024-08-31 06:43:32 +0200

    Install react-icons dependance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import { MainButton } from "@/components/MainButton";
 
 export default function Home() {
   return (
-    <main className="">
-      Hello word
+    <main className="grid grid-cols-8 gap-4 bg-[#EDEDED] h-[100vh]">
+      <div className="col-span-2 bg-white h-[100vh] p-2">
+        <div className="font-[600] w-full text-[20px] h-[100px] flex items-center">doomi</div>
+        <MainButton/>
+      </div>
+      <div className="col-span-4 bg-white h-[100vh]"></div>
+      <div className="col-span-2 bg-white h-[100vh]"></div>
     </main>
   );
 }
--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -1,5 +1,5 @@
 export const MainButton = () => {
-    return <button className="rounded-lg bg-[#D14318]">
+    return <button className="rounded-lg text-white w-full py-[3px] bg-[#D14318]">
         text
     </button>
 }
