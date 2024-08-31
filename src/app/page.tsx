47c49e19e07df99d23feb169cd5407f5fd2788e4
jospin6 <jospinndagano1@gmail.com>
2024-08-31 12:41:49 +0200

    Remove comonent bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import { MainButton } from "@/components/MainButton";
 export default function Home() {
   return (
     <main className="grid grid-cols-8 gap-4 bg-[#000000]">
-      <div className="col-span-2 bg-[#121212] h-[100vh] p-2">
+      <div className="col-span-2 h-[100vh] p-2">
         <div className="font-[600] w-full text-white text-[20px] h-[100px] flex items-center">doomi</div>
         <MainButton/>
       </div>
-      <div className="col-span-4 bg-[#121212] h-[100vh]"></div>
-      <div className="col-span-2 bg-[#121212] h-[100vh]"></div>
+      <div className="col-span-4 h-[100vh]"></div>
+      <div className="col-span-2 h-[100vh]"></div>
     </main>
   );
 }
