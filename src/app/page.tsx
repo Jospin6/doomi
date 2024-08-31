f83ed4551706da08fe7f6daa057f5563072d9373
jospin6 <jospinndagano1@gmail.com>
2024-08-31 09:43:00 +0200

    Design the app title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import { MainButton } from "@/components/MainButton";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-8 gap-4 bg-[#EDEDED] h-[100vh]">
-      <div className="col-span-2 bg-white h-[100vh] p-2">
-        <div className="font-[600] w-full text-[20px] h-[100px] flex items-center">doomi</div>
+    <main className="grid grid-cols-8 gap-4 bg-[#000000]">
+      <div className="col-span-2 bg-[#121212] h-[100vh] p-2">
+        <div className="font-[600] w-full text-white text-[20px] h-[100px] flex items-center">doomi</div>
         <MainButton/>
       </div>
-      <div className="col-span-4 bg-white h-[100vh]"></div>
-      <div className="col-span-2 bg-white h-[100vh]"></div>
+      <div className="col-span-4 bg-[#121212] h-[100vh]"></div>
+      <div className="col-span-2 bg-[#121212] h-[100vh]"></div>
     </main>
   );
 }
