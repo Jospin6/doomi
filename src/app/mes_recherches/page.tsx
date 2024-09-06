3fba569cbda1094a6822367875a94ee583add5d7
jospin6 <jospinndagano1@gmail.com>
2024-09-06 09:49:02 +0200

    Redisign the search bar comp

--- src/app/mes_recherches/page.tsx
+++ src/app/mes_recherches/page.tsx
@@ -1,7 +1,9 @@
+import { SearchBar } from "@/components/SearchBar";
+
 export default function MesRecherches() {
     return <div className=" flex w-full text-white">
         <div className="w-[70%] min-h-[100vh] h-auto border-x-[1px] border-[#121212]">
-
+            <SearchBar isHidden={true}/>
         </div>
 
         <div className="w-[30%]">
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,18 @@
 import { IoSearchOutline } from "react-icons/io5"
 import { CategoryItem } from "./CategoryItem"
 
-export const SearchBar = () => {
-    return <div className="h-[80px] border-b-[1px] border-[#121212] w-full px-2">
-    <div className="flex items-center mt-2 rounded-lg bg-[#121212] h-[35px]">
+type SearchBarProps = {
+    isHidden?: boolean
+}
+
+export const SearchBar = ({isHidden = false}: SearchBarProps) => {
+    let navbarStyle = "h-[80px] border-b-[1px] border-[#121212] w-full px-2"
+    return <div className={!isHidden ? `${navbarStyle}` : `${navbarStyle} flex items-center`}>
+    <div className="flex items-center mt-2 w-full rounded-lg bg-[#121212] h-[35px]">
       <input type="text" className="w-[95%] outline-none mr-2 bg-transparent pl-2 text-gray-500" placeholder="Rechercher sur doomi" />
       <IoSearchOutline className="text-white cursor-pointer" />
     </div>
-    <div className="h-[44px] flex items-center justify-between">
+    <div className={!isHidden ? `h-[44px] flex items-center justify-between`: `hidden`}>
       <CategoryItem title="Tous" />
       <CategoryItem title="Produits" isActive={true} />
       <CategoryItem title="Services" />
