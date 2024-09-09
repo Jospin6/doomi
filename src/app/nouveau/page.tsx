ff10dafcae02c5e40ac2eaf2702c353ed675c8a9
jospin6 <jospinndagano1@gmail.com>
2024-09-09 19:21:21 +0200

    Add categories to the new post page

--- src/app/nouveau/page.tsx
+++ src/app/nouveau/page.tsx
@@ -1,10 +1,17 @@
+import { CategoryItem } from "@/components/CategoryItem";
 import { SearchBar } from "@/components/SearchBar";
 
 export default function Nouveau() {
     return <div className=" flex w-full text-white">
         <div className="w-[70%] min-h-[100vh] h-auto border-x-[1px] border-[#121212]">
-            {/* <SearchBar isHidden={true} hideOtherComp={true} topTitle="Favories" /> */}
-            Nouveau post
+            <div className="border-b-[1px] border-[#121212]">
+                <div className={`h-[44px] flex items-center justify-between px-4`}>
+                    <CategoryItem title="Produits" isActive={true}/>
+                    <CategoryItem title="Services" />
+                    <CategoryItem title="Evenements" />
+                    <CategoryItem title="Emplois" />
+                </div>
+            </div>
         </div>
 
         <div className="w-[30%]">
