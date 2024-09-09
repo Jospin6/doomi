228a6d9def898459ae92a3741db3b7b4bdf1608c
jospin6 <jospinndagano1@gmail.com>
2024-09-09 19:36:24 +0200

    Refactore the code

--- src/app/nouveau/page.tsx
+++ src/app/nouveau/page.tsx
@@ -7,14 +7,14 @@ export default function Nouveau() {
         <div className="w-[70%] min-h-[100vh] h-auto border-x-[1px] border-[#121212]">
             <div className="border-b-[1px] border-[#121212]">
                 <div className={`h-[44px] flex items-center justify-between px-4`}>
-                    <CategoryItem title="Produits" isActive={true}/>
+                    <CategoryItem title="Produits" isActive={true} />
                     <CategoryItem title="Services" />
                     <CategoryItem title="Evenements" />
                     <CategoryItem title="Emplois" />
                 </div>
-                <div className="p-4 ">
-                    <Input type="text" id="title" placeholder="Entrer le titre" label="Quel est le titre ?"/>
-                </div>
+            </div>
+            <div className="p-4 ">
+                <Input type="text" id="title" placeholder="Entrer le titre" label="Quel est le titre ?" />
             </div>
         </div>
 
