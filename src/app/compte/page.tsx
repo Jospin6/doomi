aeb3f51a5da3482876e13bbc2dc760b0a3f90983
jospin6 <jospinndagano1@gmail.com>
2024-09-11 17:41:32 +0200

    Create a link to connexion page

--- src/app/compte/page.tsx
+++ src/app/compte/page.tsx
@@ -1,9 +1,18 @@
+import { AccountComp } from "@/components/AccountComp";
 import { SearchBar } from "@/components/SearchBar";
+import Link from "next/link";
 
 export default function Compte() {
     return <div className=" flex w-full text-white">
         <div className="w-[70%] min-h-[100vh] h-auto border-x-[1px] border-[#121212]">
             <SearchBar isHidden={true} hideOtherComp={true} topTitle="Compte" />
+            <div className="p-6">
+                <div className="border-[1px] border-[#121212] text-gray-500 rounded-lg h-[150px] mb-4">
+
+                </div>
+                <Link href={"/connexion"}><AccountComp/></Link>
+                
+            </div>
             
         </div>
 
---
+++ src/components/AccountComp.tsx
@@ -0,0 +1,8 @@
+import { IoLogOut } from "react-icons/io5"
+
+export const AccountComp = () => {
+    return <div className="border-[1px] border-[#121212] text-gray-500 rounded-lg p-4 w-[200px] ">
+        <div className="text-[25px]"><IoLogOut/></div>
+        <div className="text-[14px]">Deconnaixion</div>
+    </div>
+}
