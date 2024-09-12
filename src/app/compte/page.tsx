79b1cfed9c2d271f9ea1dd2b9878929d82211ed0
jospin6 <jospinndagano1@gmail.com>
2024-09-12 19:16:44 +0200

    Code refactoring

--- src/app/compte/page.tsx
+++ src/app/compte/page.tsx
@@ -10,10 +10,10 @@ export default function Compte() {
                 <div className="border-[1px] border-[#121212] text-gray-500 rounded-lg h-[150px] mb-4">
 
                 </div>
-                <Link href={"/connexion"}><AccountComp/></Link>
-                
+                <Link href={"/connexion"}><AccountComp /></Link>
+
             </div>
-            
+
         </div>
 
         <div className="w-[30%]">
