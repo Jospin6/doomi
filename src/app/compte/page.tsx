3b54640b4e502f1bbe3420783b83e968485eb5aa
jospin6 <jospinndagano1@gmail.com>
2024-09-17 16:21:03 +0200

    Dispact setIsTextHidden action

--- src/app/compte/page.tsx
+++ src/app/compte/page.tsx
@@ -1,8 +1,13 @@
+"use client"
 import { AccountComp } from "@/components/AccountComp";
 import { SearchBar } from "@/components/SearchBar";
+import { useDispatch } from "react-redux"
+import { AppDispatch } from "@/features/store";
+import {setIsTextHidden} from "@/features/navbarSlice"
 import Link from "next/link";
 
 export default function Compte() {
+    const dispatch = useDispatch<AppDispatch>()
     return <div className=" flex w-full text-white">
         <div className="w-[70%] min-h-[100vh] h-auto border-x-[1px] border-[#121212]">
             <SearchBar isHidden={true}/>
@@ -10,7 +15,7 @@ export default function Compte() {
                 <div className="border-[1px] border-[#121212] text-gray-500 rounded-lg h-[150px] mb-4">
 
                 </div>
-                <Link href={"/connexion"}><AccountComp /></Link>
+                <Link href={"/connexion"} onClick={() => dispatch(setIsTextHidden())}><AccountComp /></Link>
 
             </div>
 
