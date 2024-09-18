03a403df544f4322ee62dc1f1c83e268481616b0
jospin6 <jospinndagano1@gmail.com>
2024-09-18 10:44:38 +0200

    Change the categorie route name

--- src/app/[categorie]/page.tsx
+++ src/app/[categorie]/page.tsx
@@ -9,7 +9,7 @@ interface CategoriesProps {
 export default function Categorie({ params }: CategoriesProps) {
     return <div className=" flex w-full text-white">
         <div className="w-[70%] min-h-[100vh] h-auto border-x-[1px] border-[#121212]">
-            <SearchBar isHidden={true} hideOtherComp={true} topTitle={params.categorie[0]} />
+            <SearchBar isHidden={true} />
         </div>
 
         <div className="w-[30%]">
