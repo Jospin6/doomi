1cdf5b2a05e60ba6480701ff24d0d813bdf09ce4
jospin6 <jospinndagano1@gmail.com>
2024-09-28 08:19:46 +0200

    Refactor the code

--- src/components/AuthComp.tsx
+++ src/components/AuthComp.tsx
@@ -5,12 +5,12 @@ type AuthCompProps = {
     hSubTitle?: string
 }
 
-export const AuthComp = ({children, hTitle, hSubTitle}: AuthCompProps) => {
+export const AuthComp = ({ children, hTitle, hSubTitle }: AuthCompProps) => {
     return <div className='w-[350px] min-h-[400px] h-auto bg-[#121212] m-auto rounded-lg mt-[50px]  relative'>
-    <div className='text-center mb-4'>
-        <p className='text-2xl text-white py-4'>{hTitle}</p>
-        <p className='text-[12px] text-gray-500'> {hSubTitle}</p>
+        <div className='text-center mb-4'>
+            <p className='text-2xl text-white py-4'>{hTitle}</p>
+            <p className='text-[12px] text-gray-500'> {hSubTitle}</p>
+        </div>
+        {children}
     </div>
-    {children}
-</div>
 }
--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -5,9 +5,9 @@ type SelectInputProps = {
 
 }
 
-export const SelectInput = ({id, label, name}: SelectInputProps) => {
+export const SelectInput = ({ id, label, name }: SelectInputProps) => {
     return <div className="mb-4">
-         <label htmlFor={id} className="font-[500] text-[12px] block"> {label} </label>
+        <label htmlFor={id} className="font-[500] text-[12px] block"> {label} </label>
         <select name={name} id={id} className="w-full border-[1px] h-[35px] rounded-lg border-[#121212] 
             outline-none mr-2 bg-transparent pl-2 text-[12px]" >
             <option value="" className="text-gray-500">Choisi une categorie</option>
