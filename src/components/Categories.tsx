4488b36bdb3dbe5f19d386eb14c697b906a50a17
jospin6 <jospinndagano1@gmail.com>
2024-09-29 18:01:56 +0200

    Code refactoring

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,11 +1,11 @@
 import { CategoryItem } from "./CategoryItem"
 
 type CategoriesProps = {
-    isHidden?: boolean
+  isHidden?: boolean
 }
 
-export const Categories = ({isHidden} : CategoriesProps) => {
-    return <div className={!isHidden ? `h-[44px] flex items-center justify-between`: `hidden`}>
+export const Categories = ({ isHidden }: CategoriesProps) => {
+  return <div className={!isHidden ? `h-[44px] flex items-center justify-between` : `hidden`}>
     <CategoryItem title="Tous" />
     <CategoryItem title="Produits" isActive={true} />
     <CategoryItem title="Services" />
--- src/components/CategoryItem.tsx
+++ src/components/CategoryItem.tsx
@@ -4,11 +4,11 @@ type CategoryItemProps = {
     isActive?: boolean
     className?: string
 }
-export const CategoryItem = ({title, isActive = false, className}: CategoryItemProps) => {
+export const CategoryItem = ({ title, isActive = false, className }: CategoryItemProps) => {
     let style = "h-[100%] text-[14px]"
-    return <button className={ !isActive 
-                ? `${style} text-gray-500 hover:text-white ${className} hover:border-b-[4px] hover:border-[#D14318]` 
-                : `text-white border-b-[4px] ${className}  border-[#D14318] ${style}`}>
+    return <button className={!isActive
+        ? `${style} text-gray-500 hover:text-white ${className} hover:border-b-[4px] hover:border-[#D14318]`
+        : `text-white border-b-[4px] ${className}  border-[#D14318] ${style}`}>
         {title}
     </button>
 }
