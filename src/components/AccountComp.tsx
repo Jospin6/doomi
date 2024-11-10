7956a7d87715333fe99acd943b16354465afa8e8
jospin6 <jospinndagano1@gmail.com>
2024-11-10 20:11:41 +0200

    Code refactoring

--- src/components/AccountComp.tsx
+++ src/components/AccountComp.tsx
@@ -5,7 +5,7 @@ type AccountCompProps = {
     children: React.ReactNode
 }
 
-export const AccountComp = ({text, children}: AccountCompProps) => {
+export const AccountComp = ({ text, children }: AccountCompProps) => {
     return <div className="border-[1px] border-[#121212] text-gray-500 rounded-lg p-4 w-[200px] ">
         <div className="text-[25px]">{children}</div>
         <div className="text-[14px]">{text}</div>
