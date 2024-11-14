c8fd760fd1c70c505fe9edaef05bdfce4748bdc7
jospin6 <jospinndagano1@gmail.com>
2024-11-14 13:07:16 +0200

    get data from geonames

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import navbarReducer from './navbarSlice'
+import geoNamesReducer from './geoNamesSlice'
 
 export const store = configureStore({
     reducer: {
-        navbar: navbarReducer
+        navbar: navbarReducer,
+        geoNames: geoNamesReducer
     }
 })
 
