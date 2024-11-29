3170bbd6404a1e3016f13558f87892b5f47d5c96
jospin6 <jospinndagano1@gmail.com>
2024-11-29 18:23:33 +0200

    remove the header in the nouveau page

--- src/app/nouveau/page.tsx
+++ src/app/nouveau/page.tsx
@@ -1,20 +1,16 @@
 import { CategoryItem } from "@/components/CategoryItem";
 import ProduitForm from "@/components/ProduitForm";
+import { SearchBar } from "@/components/SearchBar";
 
 export default function Nouveau() {
     return <div className=" flex w-full text-white">
         <div className="w-[70%] min-h-[100vh] h-auto border-x-[1px] border-[#121212]">
             <div className="border-b-[1px] border-[#121212]">
-                <div className={`h-[44px] flex items-center justify-between px-4`}>
-                    <CategoryItem title="Produits" isActive={true} />
-                    <CategoryItem title="Services" />
-                    <CategoryItem title="Evenements" />
-                    <CategoryItem title="Emplois" />
-                </div>
+               {/* IL FAUT AJOUTER UN DESIGN ICI */}
             </div>
             <div className="p-4 ">
                 <div className="w-[60%]">
-                    <ProduitForm/>
+                    <ProduitForm />
                 </div>
             </div>
         </div>
