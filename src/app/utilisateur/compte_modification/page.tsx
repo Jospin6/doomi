75ead4d450c67c2916c8dcf14d80ab43db79a695
jospin6 <jospinndagano1@gmail.com>
2024-12-31 23:17:16 +0200

    Code refactoring

--- src/app/utilisateur/compte_modification/page.tsx
+++ src/app/utilisateur/compte_modification/page.tsx
@@ -4,12 +4,12 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 
 export default function CompteModification() {
     const user = useCurrentUser()
-    
+
     return <div className=" flex w-full text-white">
         <div className="w-[70%] min-h-[100vh] h-auto border-x-[1px] border-[#121212]">
             <SearchBar isHidden={true} />
             <div className="px-2">
-                
+
             </div>
         </div>
 
